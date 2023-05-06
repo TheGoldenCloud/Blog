ee66f69af2f6cd8be6b047f4caab5978cb69a29c
GoldenCloudPc <jvladimir54@yahoo.com>
2023-05-06 21:06:12 +0200

    Dodate rute za logovanje verifikovanje i vracanje usera

--- rute/userRuta.js
+++ rute/userRuta.js
@@ -1,10 +1,12 @@
 import express from "express"
-import { createUser } from "../kontroleri/userC.js";
+import { createUser,getUser,verifyToken,loginUser } from "../kontroleri/userC.js";
 
 let rout = express.Router();
 
-rout.route('/users').post(createUser);
+rout.route('/users').get(verifyToken).get(getUser)
 
+rout.route('/users/login').post(loginUser);
 
+rout.route('/users/create').post(createUser);
 
 export default rout;
