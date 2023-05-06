915cfe9abe5ca79ecd162cef7e878aeb2ed8c8ba
GoldenCloudPc <jvladimir54@yahoo.com>
2023-05-06 21:05:28 +0200

    Dodate funkcije za logovanje verifikovanje i vracanje usera

--- kontroleri/userC.js
+++ kontroleri/userC.js
@@ -1,8 +1,58 @@
 import "dotenv/config";
 import asyncHandler from "express-async-handler"
 import userModel from "../modeli/user.js"
+import jwt from "jsonwebtoken"
 
 //DODATI ERRORE
+
+//Proverava da li user postoji i da kreira token
+let loginUser = asyncHandler(async (req,res) =>{
+    let { username, password } = req.body;
+
+    let user = await userModel.findOne({ username: username, password: password });
+    
+    if(!user){
+        console.log(user);
+        res.status(400).json({ message: "User not found"});
+    }else{
+        let accT = jwt.sign({    
+            user:{
+                username: user.username,
+                password: user.password
+            }
+        },process.env.JWT_TOKEN,{ expiresIn: "2m"});
+        res.status(200).json(accT); 
+    }
+
+});
+
+let verifyToken = asyncHandler (async (req, res, next) => {
+    let token;
+    let oathHeader = req.headers.authorization
+    if(oathHeader == undefined){
+        res.status(401).json({
+            message: "Niste ulogovani"
+        })
+    }else{
+        if(oathHeader && oathHeader.startsWith("Bearer")){
+            token = oathHeader.split(" ")[1];
+            jwt.verify(token,process.env.JWT_TOKEN,(err,user) => {
+                if(err) {
+                    res.status(401);
+                }else{
+                    req.body = user;
+                    next();
+                }
+            });
+        }
+    }
+
+})
+
+//Varaca usera
+let getUser = asyncHandler(async (req,res)=>{   //Get users
+    res.json(req.body.user);
+})
 
 //Pravi usera
 let createUser = asyncHandler(async (req,res)=>{
@@ -23,4 +73,4 @@ let createUser = asyncHandler(async (req,res)=>{
 
 
 
-export { createUser }
+export { createUser,loginUser,getUser,verifyToken }
